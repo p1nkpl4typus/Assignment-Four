1b47cc6409a1dda115d03ae5f3f504ed97ea3ae9
p1nkpl4typus <iam@shelaghstorla.com>
2018-09-25 13:37:32 -0600

    started about.html styling

--- assets/main.js
+++ assets/main.js
@@ -1,12 +1,20 @@
 // Mobile Nav Bar
-const button = document.querySelector('#nav-btn');
+const button = document.querySelector('#nav-btn-open');
+const close = document.querySelector('#nav-btn-closed');
 const menu = document.querySelector('#menu');
 
 function openMenu() {
   menu.className = 'mobile-nav';
+  menu.setAttribute('aria-expanded', 'true');
 };
 
+function closeMenu() {
+  menu.className = 'mobile-nav closed';
+  menu.setAttribute('aria-expanded', 'false');
+}
+
 button.addEventListener('click', openMenu);
+close.addEventListener('click', closeMenu);
 
 
 
