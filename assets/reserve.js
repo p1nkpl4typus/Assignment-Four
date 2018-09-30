785d85828c93ea7bab920e7d9a180ba85769721f
Shelagh <34905807+p1nkpl4typus@users.noreply.github.com>
2018-09-30 14:50:57 -0600

    completed styling on all pages

--- assets/reserve.js
+++ assets/reserve.js
@@ -1,26 +1,40 @@
+var main = document.querySelector('.reserve-form');
+var result = document.querySelector('#reserve-details');
 var weekdaySession = document.getElementById('reserve-session');
 var weekendSession = document.getElementById('reserve-session-wkd');
 var date = document.getElementsByTagName('td');
 var sessionTime = document.getElementsByClassName('time');
-var guests = document.querySelector("#num").value + " Crafter(s)";
+var crafter = document.querySelector("#crafter");
+var reserveBtn = document.querySelector("#reserve-submit");
+var email = document.querySelector("#email");
+var dateDisplay = document.querySelector("#date-display");
+var dateDisplayWkd = document.querySelector("#date-display-wkd");
+
 
 function getDate(e) {
     var selectedDate = "October " + e.currentTarget.textContent;
     if (e.currentTarget.classList.contains('open-wkd')){
         weekendSession.style.display = 'flex';
         weekdaySession.style.display = 'none';
+        dateDisplayWkd.textContent = selectedDate;
     } else if (e.currentTarget.classList.contains('open')){
         weekdaySession.style.display = 'flex';
         weekendSession.style.display = 'none' ;
+        dateDisplay.textContent = selectedDate;
 }
     console.log(selectedDate);
 }
 
 function getTime(f) {
+    var crafterName = document.querySelector("#crafter-name").value;
+    var crafterEmail = document.querySelector("#crafter-email").value;
     var selectedTime = "Time: " + f.currentTarget.value;
     console.log(selectedTime);
+    if (crafterName != "" && crafterEmail != "") {
+        reserveBtn.removeAttribute('disabled');
+        reserveBtn.classList.remove('btn-disabled');
+    }
 }
-
 
 for (var i = 0; i < date.length; i++){
     date[i].addEventListener('click', getDate, false);
@@ -30,27 +44,14 @@ for (var j = 0; j < sessionTime.length; j++) {
     sessionTime[j].addEventListener('click', getTime, false);
 }
 
+function recap() {
+    var crafterName = document.querySelector("#crafter-name").value;
+    var crafterEmail = document.querySelector("#crafter-email").value;
+    crafter.textContent = crafterName;
+    email.textContent = crafterEmail;
+    main.style.display = 'none';
+    result.style.display = 'block';
+}
 
-
-
-
-
-
-// document.addEventListener('click', function(el) {
-//     if (el.target.classList.contains('open-wkd')){
-//         console.log(date, guests);
-//         weekendSession.style.display = 'flex';
-//         weekdaySession.style.display = 'none';
-//     } else if (el.target.classList.contains('open')){
-//         var date = "October " + el.target.textContent;
-//         console.log(date);
-//         weekdaySession.style.display = 'flex';
-//         weekendSession.style.display = 'none' ;
-//     } else if (el.target.classList.contains('time')) {
-//         var time = "Time: " + el.target.value;
-//         console.log(date, guests, time);
-//     }
-// })
-
-
+reserveBtn.addEventListener('click', recap)
 
